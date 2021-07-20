44c3a16166c8d2d907d64f3c7dede687e91b396a
mitsuji <tkms@mitsuji.org>
2021-07-20 09:16:20 +0900

    mod: change directory structure for safer deployment

--- webapp/start.sh
+++ webapp/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -cp web/WEB-INF/classes:target/mitsuji-zipnshare-0.9.1-jar-with-dependencies.jar HttpService 0.0.0.0 8080 web
+java -cp config:target/mitsuji-zipnshare-0.9.1-jar-with-dependencies.jar HttpService 0.0.0.0 8080 web
